e1a5852ffc14bf94451218182ce49e638d602e48
Prisciliano Cortés <pris98.cortes@gmail.com>
2023-10-07 23:09:10 -0600

    fix: Update test in elements with vite

--- src/HookApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const HookApp = () => {
-    return (
-        <div>
-            <h1>Hook</h1>
-        </div>
-    )
-}
--- src/components/09-useContext/ComponentError.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ComponentError = () => {
-    return (
-      <div>
-          <h5>Has llegado a un mundo desconocido</h5>
-      </div>
-    )
-}
